bb5ab89479a7f6bbaadfced95cfdfc74811deb87
Yo69sr <yousriberriche16@gmail.com>
2025-05-06 09:54:01 +0200

    ajout du profile - picture

--- bdd/setup.sql
+++ bdd/setup.sql
@@ -3,4 +3,5 @@ CREATE TABLE users (
     username VARCHAR(30) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
+    Profile-picture VARCHAR(255) '.png''.jpeg''.gif''.jpg',
 );
