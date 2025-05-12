5756fdd7082e0bbb925b82b946e8b95588ac33a2
Yousri Berriche <yousriberriche16@gmail.com>
2025-05-12 18:03:57 +0200

    ajout de la table postlike et commentlike

--- bdd/setup.sql
+++ bdd/setup.sql
@@ -2,8 +2,8 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(30) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-    Profile-picture VARCHAR(255) '.png''.jpeg''.gif''.jpg',
+    password VARCHAR(255) NOT NULL,
+    profile_picture VARCHAR(255)
 );
 
 CREATE TABLE posts (
@@ -13,23 +13,37 @@ CREATE TABLE posts (
     content VARCHAR(5000) NOT NULL,
     likes INTEGER DEFAULT 0,
     themes VARCHAR(255),
-    created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (owner_id) REFERENCES users(id),
+    created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id INTEGER NOT NULL,
     owner_id INTEGER NOT NULL,
-    content NOT NULL,
+    content TEXT NOT NULL,
+    likes INTEGER DEFAULT 0,
     created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
-CREATE TABLE identifiant(
-    id INTEGER PRIMARY KEY AUOINCREMENT,
+CREATE TABLE comment_likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     comment_id INTEGER NOT NULL,
-    PRIMARY KEY (id, comment_id),
-    FOREIGN KEY (id) REFERENCES user(id),
+    owner_id INTEGER NOT NULL,
+    created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (comment_id) REFERENCES comments(id),
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    UNIQUE (comment_id, owner_id)
+);
+
+CREATE TABLE post_likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER NOT NULL,
+    owner_id INTEGER NOT NULL,
+    created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    UNIQUE (post_id, owner_id)
 );
