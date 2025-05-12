dde7d1872b32d0d8a2ea8eb7f7d5a9dd16757060
Yousri Berriche <yousriberriche16@gmail.com>
2025-05-12 10:32:45 +0200

    ajout de la table identifiant et liaison des table

--- bdd/setup.sql
+++ bdd/setup.sql
@@ -9,11 +9,12 @@ CREATE TABLE users (
 CREATE TABLE posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     owner_id INTEGER NOT NULL,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(5000) NOT NULL,
     likes INTEGER DEFAULT 0,
-    themes TEXT,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    themes VARCHAR(255),
+    created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (owner_id) REFERENCES users(id),
 );
 
 CREATE TABLE comments (
@@ -21,6 +22,14 @@ CREATE TABLE comments (
     post_id INTEGER NOT NULL,
     owner_id INTEGER NOT NULL,
     content NOT NULL,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-)
+    created_at VARCHAR(255) DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+);
 
+CREATE TABLE identifiant(
+    id INTEGER PRIMARY KEY AUOINCREMENT,
+    comment_id INTEGER NOT NULL,
+    PRIMARY KEY (id, comment_id),
+    FOREIGN KEY (id) REFERENCES user(id),
+    FOREIGN KEY (comment_id) REFERENCES comments(id),
+);
